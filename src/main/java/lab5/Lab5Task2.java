1838258b9c7ec526327b5f1c2a699983b2f5e3be
Artem Grebenyk <a.hrebenyk@kitu.nau.edu.ua>
2026-04-01 00:01:55 +0300

    lab5 upd

--- src/main/java/lab5/Lab5Task2.java
+++ src/main/java/lab5/Lab5Task2.java
@@ -6,16 +6,22 @@ public class Lab5Task2 {
 
     static void main() {
         println("Calculate sum for m = 1, n = 1: " + calculateSum(1, 1)); // = 1
-        println("Calculate sum for m = 1, n = 5: " + calculateSum(1, 5)); // = 5
-        println("Calculate sum for m = 5, n = 1: " + calculateSum(5, 1)); // = 1 + 2 + 3 + 4 + 5
-        println("Calculate sum for m = 4, n = 4: " + calculateSum(4, 4)); // = 4 + 8 + 12 + 16
+        println("Calculate sum for m = 3, n = 2: " + calculateSum(3, 2)); // = 1 + 4 + 9
+        println("Calculate sum for m = 3, n = 3: " + calculateSum(3, 3)); // = 1 + 8 + 27
+        println("Calculate sum for m = 5, n = 2: " + calculateSum(5, 2)); // = 1 + 4 + 9 + 16 + 25
     }
 
-    //1n + 2n + 3n +  + mn
     public static long calculateSum(int m, int n) {
         long sum = 0;
+        int x;
+
         for (int i = 1; i <= m; i++) {
-            sum += (long) i * n;
+            x = 1;
+            for (int j = 1; j <= n; j++) {
+                x *= i;
+            }
+            sum += x;
         }
-        return sum;    }
+        return sum;
+    }
 }
